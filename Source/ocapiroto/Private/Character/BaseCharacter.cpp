5320c372d84d2a9ce4db50385b46656dad630695
strubloid <strubloid@gmail.com>
2024-07-09 17:26:26 +0100

    Creation of the first character

--- Source/ocapiroto/Private/Character/BaseCharacter.cpp
+++ Source/ocapiroto/Private/Character/BaseCharacter.cpp
@@ -7,6 +7,11 @@ ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	// Creating the weapon, attaching to the object and removing the collision
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 // Called when the game starts or when spawned
